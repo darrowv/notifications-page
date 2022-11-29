64958156eec682d79640210b64cb023dda4b009d
Muhammad-Nasyr <nasirmaga45@gmail.com>
2022-11-29 20:29:57 +0300

    layout finished

---
+++ assets/notifications.js
@@ -0,0 +1,60 @@
+const notifications = [
+  {
+    name: "Mark Webber",
+    date: "1m",
+    img: "./assets/images/avatar-mark-webber.webp",
+    status: "unread",
+    action: "reacted to your recent post",
+    subject: "My first tournament today!",
+  },
+  {
+    name: "Angela Gray",
+    date: "5m",
+    img: "./assets/images/avatar-angela-gray.webp",
+    status: "unread",
+    action: "followed you",
+  },
+  {
+    name: "Jacob Thompson",
+    date: "1 day",
+    img: "./assets/images/avatar-jacob-thompson.webp",
+    status: "unread",
+    action: "has joined your group",
+    subject: "Chess Club",
+  },
+  {
+    name: "Rizky Hasanuddin",
+    date: "5 days",
+    img: "./assets/images/avatar-rizky-hasanuddin.webp",
+    status: "read",
+    action: "sent you a private message",
+    message:
+      "Hello, thanks for setting up the Chess Club. I've been a member for a few weeks now and I'm already having lots of fun and improving my game.",
+  },
+  {
+    name: "Kimberly Smith",
+    date: "1 week",
+    img: "./assets/images/avatar-kimberly-smith.webp",
+    status: "read",
+    action: "commented on your picture",
+    picture: "./assets/images/image-chess.webp",
+  },
+  {
+    name: "Nathan Peterson",
+    date: "2 weeks",
+    img: "./assets/images/avatar-nathan-peterson.webp",
+    status: "read",
+    action: "reacted to your recent post",
+    subject: "5 end-game strategies to increase your win rate",
+  },
+  {
+    name: "Anna Kim",
+    date: "2 weeks",
+    img: "./assets/images/avatar-anna-kim.webp",
+    status: "read",
+    action: "left the group",
+    subject: "Chess Club",
+  },
+];
+
+export default notifications;
---
+++ script.js
@@ -0,0 +1,13 @@
+import notices from "./assets/notifications.js";
+
+
+// for (let notice of notices) {
+//   const img = document.createElement("img");
+//   img.src = notice.img;
+//   img.alt = notice.name;
+
+//   // const body = document.getElementsByTagName("body");
+
+//   document.body.append(img)
+//   console.log(notice.img);
+// }
